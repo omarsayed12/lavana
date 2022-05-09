0ce617914b143ed58445608546bcd373be327d9c
omarsayed12 <omarsayed40400@gmail.com>
2022-05-10 00:50:28 +0200

    uploading html structure

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,36 +1,44 @@
 $('.autoplay').slick({
+    slidesToShow: 12,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    rtl: true
+});
+$('.offer-days').slick({
     slidesToShow: 4,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 3000,
-    mobileFirst: true,
-    arrows: false,
-    responsive: [{
-      breakpoint: 1000,
-      settings: {
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        arrows: false,
-        dots: false
-      }
-    },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-          arrows: false,
-          dots: true
-        }
-      },
-      ,{
-     breakpoint: 319,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          arrows: false,
-          dots: true,
-          centerMode: false,
-        }
-    }]
+    autoplaySpeed: 2000,
+    rtl: true
 });
+$('.electronicOffers-cart').slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    rtl: true
+});
+$('.womenFashion-cart').slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    rtl: true
+});
+$('.productDiscount-content').slick({
+    slidesToShow: 9,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    rtl: true
+});
+
+$('.manFashion-cart').slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    rtl: true
+});
+
