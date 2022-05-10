c830fc9b486ff05bb43a302539269378c6b5188a
omarsayed12 <omarsayed40400@gmail.com>
2022-05-10 11:11:16 +0200

    add style to some sections

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,8 +3,69 @@ $('.autoplay').slick({
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
-    rtl: true
+    rtl: true,
+    mobileFirst: true,
+    arrows: false,
+    responsive: [{
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 12,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: false
+      }
+    },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 10,
+          slidesToScroll: 1,
+          arrows: false,
+          dots: true
+        }
+      },
+        , {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 7,
+                slidesToScroll: 1,
+                arrows: false,
+                dots: true,
+                centerMode: false,
+            },
+        },
+        {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 5,
+                slidesToScroll: 1,
+                arrows: false,
+                dots: true,
+                centerMode: false,
+            }
+        },
+        {
+            breakpoint: 425,
+            settings: {
+                slidesToShow: 4,
+                slidesToScroll: 1,
+                arrows: false,
+                dots: true,
+                centerMode: false,
+            }
+        },{
+        breakpoint: 319,
+        settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            arrows: false,
+            dots: true,
+            centerMode: false,
+        }
+    }]
 });
+
+
 $('.offer-days').slick({
     slidesToShow: 4,
     slidesToScroll: 1,
