7aa64eff7000068d43436f21b03813d8a25dea1d
omarsayed12 <omarsayed40400@gmail.com>
2022-05-12 08:52:17 +0200

    add style to homepage

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,10 +2,11 @@ $('.autoplay').slick({
     slidesToShow: 12,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 2000,
+    autoplaySpeed: 1500,
     rtl: true,
     mobileFirst: true,
     arrows: false,
+    infinite: true,
     responsive: [{
       breakpoint: 1200,
       settings: {
@@ -17,12 +18,10 @@ $('.autoplay').slick({
     },
       {
         breakpoint: 1024,
-        settings: {
           slidesToShow: 10,
           slidesToScroll: 1,
           arrows: false,
           dots: true
-        }
       },
         , {
             breakpoint: 768,
@@ -64,42 +63,225 @@ $('.autoplay').slick({
         }
     }]
 });
-
-
-$('.offer-days').slick({
+$('.offerDays-slide').slick({
     slidesToShow: 4,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
-    rtl: true
+    dots: true,
+    rtl: true,
+    mobileFirst: true,
+    arrows: false,
+    responsive: [{
+        breakpoint: 1200,
+        settings: {
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            arrows: false,
+            dots: false
+        }
+    },
+        {
+            breakpoint: 991,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                arrows: false,
+            }
+        }
+        ,{
+        breakpoint: 767,
+        settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            arrows: false,
+        },
+        },
+        ,{
+            breakpoint: 592,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                arrows: false,
+            },
+            },
+        {
+        breakpoint: 319,
+        settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+        }
+    }]
 });
-$('.electronicOffers-cart').slick({
+$('.offer-days').slick({
     slidesToShow: 6,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
-    rtl: true
-});
-$('.womenFashion-cart').slick({
-    slidesToShow: 6,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    rtl: true
+    dots: true,
+    rtl: true,
+    mobileFirst: true,
+    arrows: false,
+    responsive: [{
+        breakpoint: 1200,
+        settings: {
+            slidesToShow: 6,
+            slidesToScroll: 1,
+            arrows: false,
+            dots: false
+        }
+    },
+        {
+            breakpoint: 991,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                arrows: false,
+            }
+        }
+        ,{
+        breakpoint: 767,
+        settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            arrows: false,
+        },
+        },
+        ,{
+            breakpoint: 592,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                arrows: false,
+            },
+            },
+        {
+        breakpoint: 319,
+        settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+        }
+    }]
 });
 $('.productDiscount-content').slick({
-    slidesToShow: 9,
+    slidesToShow: 12,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
-    rtl: true
+    rtl: true,
+    mobileFirst: true,
+    arrows: false,
+    responsive: [{
+        breakpoint: 1400,
+        settings: {
+            slidesToShow: 9,
+            slidesToScroll: 1,
+            arrows: false,
+            dots: false
+        }
+    },{
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 8,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: false
+      }
+    },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 7,
+          slidesToScroll: 1,
+          arrows: false,
+          dots: true
+        }
+      },
+        , {
+            breakpoint: 886,
+            settings: {
+                slidesToShow: 5,
+                slidesToScroll: 1,
+                arrows: false,
+                dots: true,
+                centerMode: false,
+            },
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                arrows: false,
+                dots: true,
+                centerMode: false,
+            }
+        },
+        {
+            breakpoint: 440,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                arrows: false,
+                dots: true,
+                centerMode: false,
+            }
+        },{
+        breakpoint: 319,
+        settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            arrows: false,
+            dots: true,
+            centerMode: false,
+        }
+    }]
 });
-
-$('.manFashion-cart').slick({
-    slidesToShow: 6,
+$('.brand-slides').slick({
+    slidesToShow: 4,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
-    rtl: true
+    rtl: true,
+    mobileFirst: true,
+    arrows: false,
+    responsive: [{
+        breakpoint: 1200,
+        settings: {
+            slidesToShow: 5,
+            slidesToScroll: 1,
+            arrows: false,
+            dots: true
+        }
+    },
+    {
+        breakpoint: 991,
+        settings: {
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            arrows: false,
+            dots: true
+        },
+        },
+        {
+        breakpoint: 572,
+        settings: {
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            arrows: false,
+            dots: true
+        },
+        },
+        {
+        breakpoint: 319,
+        settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            arrows: false,
+            dots: true
+        },
+        }
+    ]
 });
-
